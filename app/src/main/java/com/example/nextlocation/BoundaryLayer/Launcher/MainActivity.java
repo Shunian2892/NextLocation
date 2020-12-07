dbeacf786a49e28f113155a785a664db61b4644b
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-12-07 15:42:14 +0100

    [Add]: navigation between fragments

--- app/src/main/java/com/example/nextlocation/BoundaryLayer/Launcher/MainActivity.java
+++ app/src/main/java/com/example/nextlocation/BoundaryLayer/Launcher/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.nextlocation.BoundaryLayer.Launcher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -13,35 +15,47 @@ import com.example.nextlocation.BoundaryLayer.UI.SettingsFragment;
 import com.example.nextlocation.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private Fragment selectedFragment = null;
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.menu_map:
+                    selectedFragment = fragmentList.get(0);
+                    break;
+                case R.id.menu_list:
+                    selectedFragment = fragmentList.get(1);
+                    break;
+                case R.id.menu_settings:
+                    selectedFragment = fragmentList.get(2);
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, selectedFragment).commit();
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpFragmentList();
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new MapFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragmentList.get(0)).commit();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-            switch (item.getItemId()){
-                case R.id.menu_map:
-                    //selectedFragment = new MapFragment();
-                    break;
-                case R.id.menu_list:
-                    //selectedFragment = new POIListFragment();
-                    break;
-                case R.id.menu_settings:
-                    //selectedFragment = new SettingsFragment();
-                    break;
-            }
-            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment, selectedFragment).commit();
-            return true;
-        }
-    };
+    private void setUpFragmentList(){
+        this.fragmentList.add(new MapFragment());
+        this.fragmentList.add(new POIListFragment());
+        this.fragmentList.add(new SettingsFragment());
+    }
 }
--- app/src/main/java/com/example/nextlocation/BoundaryLayer/UI/POIListFragment.java
+++ app/src/main/java/com/example/nextlocation/BoundaryLayer/UI/POIListFragment.java
@@ -1,5 +1,6 @@
 package com.example.nextlocation.BoundaryLayer.UI;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
