7604d1292ccbc23dfe7543b7ebdf50f3f5ff5149
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-12-07 13:22:22 +0100

    [Add]: bottomnavigationView and xml-files added

---
+++ app/src/main/java/com/example/nextlocation/BoundaryLayer/Launcher/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.nextlocation.BoundaryLayer.Launcher;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nextlocation.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private final int duration = 2000;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, duration);
+    }
+}
